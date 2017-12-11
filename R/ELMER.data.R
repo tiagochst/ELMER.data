4e4723440d15615badac4e74c7e2a9120aeb5b16
Tiago Chedraoui Silva <tiagochst@gmail.com>
2017-12-10 22:07:00 -0200

    Update doc

--- R/ELMER.data.R
+++ R/ELMER.data.R
@@ -17,19 +17,16 @@
 #'     follwing command: \code{browseVignettes("ELMER.data")}
 #'
 #' @docType package
+#' @seealso \code{\link[ELMER.data]{EPIC.manifest}}, \code{\link[ELMER.data]{EPIC.manifest.hg38}}, 
+#' \code{\link[imudata]{hm450.manifest}}, \code{\link[ELMER.data]{hm450.manifest.hg38}}, 
+#' \code{\link[ELMER.data]{Probes.motif.hg19.450K}}, \code{\link[ELMER.data]{Probes.motif.hg38.450K}}, 
+#' \code{\link[ELMER.data]{Probes.motif.hg38.EPIC}}, \code{\link[ELMER.data]{Probes.motif.hg19.EPIC}}, 
+#' \code{\link[ELMER.data]{TF.subfamily}}, \code{\link[ELMER.data]{TF.family}}, and \code{\link[ELMER.data]{TF.meth.cor}}
 #' @name ELMER.data
 #' @exportPattern ^[[:alpha:]]+
 #' @keywords utilities
 #' @examples
-#' data("Probes.motif.hg38.EPIC")
-#' data("Probes.motif.hg19.EPIC")
-#' data("Probes.motif.hg38.450K")
-#' data("Probes.motif.hg19.450K")
-#' data("EPIC.manifest")
-#' data("EPIC.manifest.hg38")
-#' data("hm450.manifest")
-#' data("hm450.manifest.hg38")
-#' data("elmer.data.example")
+#' # Please see the datasets
 NULL
 
 #' A GRanges containing hg19 annotation with suggested overall masking for EPIC platform
@@ -40,7 +37,7 @@ NULL
 #' @format A GRanges with 866895 elements
 #' @examples
 #' data("EPIC.manifest")
-NULL
+"EPIC.manifest"
 
 #' A GRanges containing hg38 annotation with suggested overall masking for EPIC platform
 #' @docType data
@@ -50,7 +47,7 @@ NULL
 #' @format A GRanges with 866895 elements
 #' @examples
 #' data("EPIC.manifest.hg38")
-NULL
+"EPIC.manifest.hg38"
 
 #' A GRanges containing hg38 annotation with suggested overall masking for hm450 platform
 #' @docType data
@@ -60,7 +57,7 @@ NULL
 #' @format A GRanges with 485577 elements
 #' @examples
 #' data("hm450.manifest.hg38")
-NULL
+"hm450.manifest.hg38"
 
 #' A GRanges containing hg19 annotation with suggested overall masking for hm450 platform
 #' @docType data
@@ -69,8 +66,10 @@ NULL
 #' @import GenomicRanges
 #' @format A GRanges with 485577 elements
 #' @examples
-#' data("hm450.manifest")
-NULL
+#' \dontrun{
+#'  data("hm450.manifest")
+#'}
+"hm450.manifest"
 
 #' A matrix with 1 if the probe (row) has a motif (column)
 #' @description 
@@ -90,8 +89,10 @@ NULL
 #' @name Probes.motif.hg38.EPIC
 #' @format A matrix with  838881 rows and 640 columns
 #' @examples
-#' data("Probes.motif.hg38.EPIC")
-NULL
+#' \dontrun{
+#'   data("Probes.motif.hg38.EPIC")
+#' }
+"Probes.motif.hg38.EPIC"
 
 #' A matrix with 1 if the probe (row) has a motif (column)
 #' @description 
@@ -111,8 +112,10 @@ NULL
 #' @name Probes.motif.hg19.EPIC
 #' @format A matrix with  838881 rows and 640 columns
 #' @examples
-#' data("Probes.motif.hg19.EPIC")
-NULL
+#' \dontrun{
+#'  data("Probes.motif.hg19.EPIC")
+#' }
+"Probes.motif.hg19.EPIC"
 
 #' A matrix with 1 if the probe (row) has a motif (column)
 #' @description 
@@ -132,8 +135,10 @@ NULL
 #' @name Probes.motif.hg19.450K
 #' @format A matrix with  466007 rows and 640 columns
 #' @examples
+#' \dontrun{
 #' data("Probes.motif.hg19.450K")
-NULL
+#' }
+"Probes.motif.hg19.450K"
 
 #' A matrix with 1 if the probe (row) has a motif (column)
 #' @description 
@@ -153,8 +158,10 @@ NULL
 #' @name Probes.motif.hg38.450K
 #' @format A matrix with  466007 rows and 640 columns
 #' @examples
+#' \dontrun{
 #' data("Probes.motif.hg38.450K")
-NULL
+#' }
+"Probes.motif.hg38.450K"
 
 
 #' A MultiAssayExperiment containing
@@ -167,8 +174,10 @@ NULL
 #' @name data
 #' @format A MultiAssayExperiment for 234 Samples (8 normal samples, 226 Primary solid tumor)
 #' @examples
+#' \dontrun{
 #' data("elmer.data.example")
-NULL
+#' }
+"data"
 
 #' A MultiAssayExperiment containing
 #' DNA methylation data: 16 promoter probes from platform 450K 
@@ -180,8 +189,10 @@ NULL
 #' @name mae.promoter
 #' @format A MultiAssayExperiment for 234 Samples (8 normal samples, 226 Primary solid tumor)
 #' @examples
+#' \dontrun{
 #' data("elmer.data.example.promoter")
-NULL
+#' }
+"mae.promoter"
 
 #' A matrix containing DNA methylation beta-values from TCGA
 #' DNA methylation data: 1728 probes
@@ -190,7 +201,7 @@ NULL
 #' @keywords internal
 #' @name Meth
 #' @format A MultiAssayExperiment for 268 Samples  and 1728 probes
-NULL
+"Meth"
 
 #' A matrix containing gene expression data from TCGA
 #' Gene Expression data: 3842 genes
@@ -199,17 +210,8 @@ NULL
 #' @keywords internal
 #' @name GeneExp
 #' @format A gene expression matrix for 234 Samples and 3842 genes
-NULL
+"GeneExp"
 
-
-#' A matrix containing the relashionship between the DNA methylation level at the probes
-#' for a given motif and the gene expression of TF.
-#' This data is used in the examples of ELMER package
-#' @docType data
-#' @keywords internal
-#' @name TF.meth.cor
-#' @format A matrix with 1968 rows (TFs) and 12 columns (motifs)
-NULL
 
 #' A list of 641 motifs with TF families (with similar bidings) from TFClass
 #' Created with the following function from ELMER pacakge 
@@ -218,7 +220,7 @@ NULL
 #' @keywords internal
 #' @name TF.family
 #' @format A list of 641 motifs with TF families (with similar bidings)
-NULL
+"TF.family"
 
 #' A list of 641 motifs with TF subfamilies (with similar bidings) from TFClass
 #' Created with the following function from ELMER pacakge 
@@ -227,4 +229,4 @@ NULL
 #' @keywords internal
 #' @name TF.subfamily
 #' @format A list of 641 motifs with TF subfamilies (with similar bidings)
-NULL
+"TF.subfamily"
